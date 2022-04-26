841435af109c5a33ce0248ae553f686db879c365
sanich123 <aovoronin.piano@gmail.com>
2022-04-26 11:02:26 +0300

    Last things with typescript

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,11 +1,4 @@
-import { ReactComponent as IconAllQuests } from '../assets/img/icon-all-quests.svg';
-import { ReactComponent as IconAdventures } from '../assets/img/icon-adventures.svg';
-import { ReactComponent as IconHorrors } from '../assets/img/icon-horrors.svg';
-import { ReactComponent as IconMystic } from '../assets/img/icon-mystic.svg';
-import { ReactComponent as IconDetective } from '../assets/img/icon-detective.svg';
-import { ReactComponent as IconScifi } from '../assets/img/icon-scifi.svg';
-
-export const genres = ['Все квесты','Приключения', 'Ужасы', 'Мистика', 'Детектив', 'Sci-fi'];
+import { IconAllQuests, IconAdventures, IconHorrors, IconMystic, IconDetective, IconScifi } from '../components/svg/svg';
 
 export const genresList = {
   allQuests: 'Все квесты',
